08205c0f60ea053a7579ecb0f5f62917e8dfe897
Brant Wellman <BrantWellman@gmail.com>
2016-01-12 15:43:10 -0700

    Add cart, add items, and display functionality

--- app/models/duffel.rb
+++ app/models/duffel.rb
@@ -7,6 +7,28 @@ class Duffel
 
   def add_item(item_id)
     contents[item_id.to_s] ||= 0
-    contents[item_id.to_s] += 0
+    contents[item_id.to_s] += 1
+  end
+
+  def total
+    contents.values.sum
+  end
+
+  def count_of(item_id)
+    contents[item_id.to_s]
+  end
+
+  def subtotal
+    item_prices = contents.map do |item_id, quantity|
+      item = Item.find(item_id)
+      item.price * quantity
+    end
+    item_prices.sum
+  end
+
+  def duffel_items
+    contents.map do |item_id, quantity|
+      Item.find(item_id)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  get "/categories/:title", to: "categories#show"
+  # get "/categories/:title", to: "categories#show"
+  resources :categories, only: [:show], param: :title
   resources :duffel_items, only: [:create]
+  get "/duffel", to: "duffel_items#index"
 end
