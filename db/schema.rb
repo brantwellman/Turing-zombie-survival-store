6d0eeb51a807a03e8766b97f8e0c687e5017ab3d
PenneyGadget <dreamofgreen@gmail.com>
2016-01-11 18:13:38 -0700

    items can be viewed under categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111232016) do
+ActiveRecord::Schema.define(version: 20160112004812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +29,10 @@ ActiveRecord::Schema.define(version: 20160111232016) do
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
+  add_index "items", ["category_id"], name: "index_items_on_category_id", using: :btree
+
+  add_foreign_key "items", "categories"
 end
--- test/factories.rb
+++ test/factories.rb
@@ -6,8 +6,16 @@ FactoryGirl.define do
     image "https://www.wpclipart.com/weapons/axe/Axe_red.svg"
   end
 
+  factory :category do
+    title
+
+    factory :category_with_items do
+      items { create_list(:item, 2) }
+    end
+  end
+
   sequence :title do |n|
-    "Title #{n}"
+    "Title#{n}"
   end
 
   sequence :description do |n|
