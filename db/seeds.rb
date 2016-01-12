1fe3a0352a19f552530fc5b24b32bb9b2713c82e
Brant Wellman <BrantWellman@gmail.com>
2016-01-12 11:00:24 -0700

    Change item-price column to integer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# class Seed
+#   def self.start
+#     new.generate
+#   end
 #
-# Examples:
+#   def generate
+#     create_items
+#     create_categories
+#   end
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   def create_categories
+#     titles = ["Stylez", "Grub", "Weapons", "Survival Gear"]
+#     titles.each do |title|
+#       Category.create(title: title)
+#     puts "#{Category.all.map(&:title).join(", ")} created."
+#     end
+#   end
+#
+#   def create_items
+#     items.each do |title, price, description, image, category_id|
+#       Item.create(title: title, price: price, description: description, image: image, category_id: category_id)
+#     end
+#   end
+#
+#   private
+#
+#   def items
+#     [
+#       {
+#       title: "axe1",
+#       price: 10.5,
+#       description: "Use it to chop your way through the crowds",
+#       image: "https://www.wpclipart.com/weapons/axe/Axe_red.svg",
+#       category_id: 1
+#       }
+#       {
+#       title: "axe2",
+#       price: 12.5,
+#       description: "It's even better! Use it to chop your way through the crowds",
+#       image: "https://www.wpclipart.com/weapons/axe/Axe_red.svg",
+#       category_id: 1
+#       },
+#       {
+#       title: "vest1",
+#       price: 13.5,
+#       description: "Not only does it look good, but you can hold lots of ammo in the pockets!",
+#       image: "https://www.wpclipart.com/weapons/axe/Axe_red.svg",
+#       category_id: 2},
+#       {
+#       title: "vest2",
+#       price: 14.5,
+#       description: "This vest has even more pockets! You can carry up to 2 days worth of food if necessary!",
+#       image: "https://www.wpclipart.com/weapons/axe/Axe_red.svg",
+#       category_id: 2
+#       }
+#     ]
+#   end
+# end
+# Seed.start
