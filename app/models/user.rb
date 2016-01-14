a19d9521f57f7c26eb9465d186b72e05aeff3ce3
PenneyGadget <dreamofgreen@gmail.com>
2016-01-13 18:21:11 -0700

    render styled flash notices

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, uniqueness: true
+  validates_presence_of :first_name,
+                        :last_name,
+                        :email,
+                        :password,
+                        :password_confirmation
 end
---
+++ test/integration/user_sees_error_with_empty_field_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class UserSeesErrorWithEmptyOrIncorrectFieldTest < ActionDispatch::IntegrationTest
+  test "user does not provide email" do
+    visit new_user_path
+
+    fill_in "First name", with: "Penney"
+    fill_in "Last name", with: "Gadget"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Submit"
+
+    assert page.has_content? "Email can't be blank"
+    assert_equal users_path, current_path
+  end
+
+  test "password must be correct" do
+    user = create(:user)
+
+    visit login_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "wrongpassword"
+    click_button "Login"
+
+    assert page.has_content? "Your email or password is incorrect"
+    assert_equal login_path, current_path
+  end
+end
