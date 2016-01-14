746eff508b63ffd16d8abef3f71a448d41caa941
James Crockett <jecrockett@gmail.com>
2016-01-14 15:27:33 -0700

    Finalize minor waffle issue updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :all_categories, :current_user
   before_action :set_duffel
