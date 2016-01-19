bf76aef6a851434c5801ecade08e245f72c8b441
James Crockett <jecrockett@gmail.com>
2016-01-19 12:13:13 -0700

    Add admin ability to select item image during creation

--- test/integration/admin_creates_new_item_test.rb
+++ test/integration/admin_creates_new_item_test.rb
@@ -5,7 +5,7 @@ class AdminCreatesNewItemTest < ActionDispatch::IntegrationTest
     Category.create(title: "Books")
     admin = create_admin
     login(admin)
-    image_path =
+    default_image =
       "http://www.vbdl.org/wp-content/uploads/2015/09/cartoon-zombies-373177.jpg"
     visit admin_dashboard_path
     click_on "Create New Item"
@@ -19,7 +19,34 @@ class AdminCreatesNewItemTest < ActionDispatch::IntegrationTest
     within "#item-show" do
       assert page.has_content? "Baseball Bat"
       assert page.has_content? "Home run!"
-      assert page.has_css? "img[src=\"#{image_path}\"]"
+      assert page.has_css? "img[src=\"#{default_image}\"]"
+    end
+  end
+
+  test "admin can upload image with new item" do
+    Category.create(title: "Books")
+    admin = create_admin
+    login(admin)
+    default_image =
+      "http://www.vbdl.org/wp-content/uploads/2015/09/cartoon-zombies-373177.jpg"
+    item_image =
+      "http://i465.photobucket.com/albums/rr16/xXswampyXx/BaseballBatNNails3_zps9660d65c.jpg"
+
+    visit admin_dashboard_path
+    click_on "Create New Item"
+    fill_in "Title", with: "Baseball Bat"
+    fill_in "Image", with: item_image
+    fill_in "Description", with: "Home run!"
+    fill_in "Price", with: 20
+    select "Books", from: "item[category_id]"
+    click_on "Submit"
+
+    assert_equal item_path(Item.last), current_path
+    within "#item-show" do
+      assert page.has_content? "Baseball Bat"
+      assert page.has_content? "Home run!"
+      assert page.has_css? "img[src=\"#{item_image}\"]"
+      refute page.has_css? "img[src=\"#{default_image}\"]"
     end
   end
 end
