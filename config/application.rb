acda1d28110a91d9d40be175cc3dcb627d1a6abc
PenneyGadget <dreamofgreen@gmail.com>
2016-01-20 09:44:23 -0700

    add rules page

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module ZombieSurvivalStore
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assests.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
