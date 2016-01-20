b3b2d215c2fff7b3569d61b6fe339df7562b23f8
Brant Wellman <BrantWellman@gmail.com>
2016-01-20 15:59:08 -0700

    Add checkbox to new user form

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,8 +2,4 @@ class MapsController < ApplicationController
   def show
     @safe_houses = User.safe_houses
   end
-
-  def index
-
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    # binding.pry
     @user = User.create(user_params)
     if @user.save
       session[:user_id] = @user.id
@@ -43,6 +44,7 @@ class UsersController < ApplicationController
       :city,
       :state,
       :zipcode,
+      :safe_house,
       :password,
       :password_confirmation)
   end
